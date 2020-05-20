2c6963479bf8318a2ff6663649ff0c9ec59466f8
elena23n <elena23n@users.noreply.github.com>
2020-05-20 17:05:44 +0300

    game

--- game/src/index.js
+++ game/src/index.js
@@ -4,7 +4,4 @@ import Game from './App';
 import * as serviceWorker from './serviceWorker';
 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
